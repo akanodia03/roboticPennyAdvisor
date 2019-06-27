1c72fcf201981b13fa6a7f6e2b51af871e28798b
kumaran <kumarnbecse@gmail.com>
2019-06-27 18:51:09 +0530

    Removed the existing folder

--- PennyStore/api/helloWorldController.py
+++
@@ -1,8 +0,0 @@
-from flask_restful import Resource
-import logging as logger
-
-class HelloWorldController(Resource):
-
-    def helloWorld(self,name):
-        logger.debug("inside setupPayment method")
-        return {"message":"Inside post method"+name},200
--- PennyStore/mysqlConn.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-mydb = mysql.connector.connect( host="localhost", user="root", passwd="goodwill", database="penny_store")
-
-myCursor = mydb.cursor()
-myCursor.execute("select * from payments")
-result = myCursor.fetchall()
-for i in result:
-    print(i)
